0512e427782b5259eeb30fffb3353960ca94340a
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-04 16:19:53 -0500

    many to many association - implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_171539) do
+ActiveRecord::Schema.define(version: 2022_04_04_174853) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2022_04_04_171539) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
