0d47686b15cdca0175e4699f38b3f7bf65364d26
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-04 12:08:37 -0500

    integration test and feature

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -2,11 +2,16 @@ require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
 
+  setup do 
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end 
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success 
     assert_difference 'Category.count', 1 do 
-      post categories_path, params: {category: {name: "Sports"}}
+      post categories_path, params: {category: {name: "Sports"} }
       assert_response :redirect 
     end
     follow_redirect!
